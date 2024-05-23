3b232e66babc787fe3f788dafaf0a4d80194cd21
Oguz Toragay <ozt0008@auburn.edu>
2024-05-23 19:30:52 -0400

    May23
    Signed-off-by: Oguz Toragay <ozt0008@auburn.edu>

---
+++ Combined/main_code.py
@@ -0,0 +1,26 @@
+import greedy
+import nl_hexaly
+import nl_pyomo_model
+import pyomo_model
+from random_instance import generate
+from random_instance import mprint
+
+def run():
+    print("jjjjjjjjjjjjjjjjjj")
+    nb_drones = 5
+    city = 'SB'
+    ins = generate(ndrones=nb_drones, condition=city)
+    incumbent = ins2incumbent(ins)
+
+def ins2incumbent(ins):
+    nb_drones = ins[2]
+    nb_slots = ins[3]
+    t_matrix = ins[4]
+    return None
+
+def incumbent2pyomo(incumbent):
+    return None
+
+if __main__:
+
+    run()
