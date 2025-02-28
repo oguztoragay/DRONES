9eb6da2a9fecaef2362c876327929781b408f2d3
Oguz Toragay <ozt0008@auburn.edu>
2025-02-27 22:47:46 -0500

    Feb27

--- Comparison_modify/main_code_modi.py
+++ Comparison_modify/main_code_modi.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     num_slots = [7, 6, 4, 3, 2]
     collective_data = pd.DataFrame(columns=['city','Iter','drones','slots','lp_var','lp_cons','lp_obj','lp_time','nlp_var','nlp_cons','nlp_obj','nlp_time'])
     for i in range(len(num_slots)):
-        instance_ = [num_drones[i], 'SB', num_slots[i], 120]
+        instance_ = [num_drones[i], 'RS', num_slots[i], 120]
         for iter_ in range(10):
             seed1 = random.randrange(sys.maxsize)
             random.seed(seed1)
