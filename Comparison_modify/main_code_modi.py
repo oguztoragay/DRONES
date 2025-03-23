52ad07059fa4f3ff32e2fbe0c1903b676aa38a66
Oguz Toragay <ozt0008@auburn.edu>
2025-03-23 12:09:41 -0400

    Mar23
    Signed-off-by: Oguz Toragay <ozt0008@auburn.edu>

--- Comparison_modify/main_code_modi.py
+++ Comparison_modify/main_code_modi.py
@@ -2,17 +2,12 @@ from datetime import date
 import os
 import random
 import sys
-from Comparison_modify import instance_gen6
+from instance_gen6 import generate
 from instance_gen7 import generate1
 from nl_pyomo_modi import nl_pyo
 from lp_pyomo_modi4 import lp_pyo
 from nl_gurobipy import nl_gurobipy
-
-# from nl_hexaly2 import hexa
 from final_hexa import hexa
-
-from nl_hexaly import hexa
-
 from pyomo.environ import value
 import pickle
 import numpy as np
@@ -21,33 +16,15 @@ import pandas as pd
 
 def run(city, verbose, seed):
     a, b, c, d = city
-    # ins = random_instance.generate(ndrones=a, city=b, slot=c, charge=d, itimes=e)
-
-    # ins = instance_gen6.generate(ndrones=a, city=b, slot=c, charge=d)
-    # lp_pyo(ins, verbose)
-    # nl_pyo(ins, verbose)
-    # nl_gurobipy(ins, verbose)
     hexa_ins = generate1(ndrones=a, city=b, slot=c, charge=d, hexa_data_required=1, seed=seed)
     gen_seq = []
     gen_st = []
     gen_ct = []
     bres = []
     hexa(hexa_ins, gen_seq, gen_st, gen_ct, 300, bres, verbose)
-    ins = instance_gen6.generate(ndrones=a, city=b, slot=c, charge=d, seed=seed)
-    lp_pyo(ins, verbose)
-    nl_pyo(ins, verbose)
-
-    ins = instance_gen6.generate(ndrones=a, city=b, slot=c, charge=d)
-    lp_pyo(ins, verbose)
-    nl_pyo(ins, verbose)
-    # nl_gurobipy(ins, verbose)
-    # gen_seq = []
-    # gen_st = []
-    # gen_ct = []
-    # bres = []
-    # hexa(ins[-1], gen_seq, gen_st, gen_ct, 60, bres, verbose)
-
-
+    ins = generate(ndrones=a, city=b, slot=c, charge=d, seed=seed)
+    # lp_pyo(ins, verbose)
+    # nl_pyo(ins, verbose)
 
 def compare(instance, report, collective_report):
     nlp_pickle = open('nlp.pickle', 'rb')
@@ -214,35 +191,27 @@ def compare(instance, report, collective_report):
         ins_data = {'lp_var': lp_[3], 'lp_cons': lp_[4], 'lp_obj': round(value(lp_[0].obj_func), 3), 'lp_time': round(lp_[1].Solver.Wall_time, 3),
                     'nlp_var': nlp_[3], 'nlp_cons': nlp_[4], 'nlp_obj': round(value(nlp_[0].obj_func), 3), 'nlp_time': round(nlp_[1].Solver.Wall_time, 3),
                     'hx_obj': h_obj_value, 'hx_time': h_sol_time, 'i_max': lp_[2][5]}
-
-        ins_data = {'lp_var': lp_[3], 'lp_cons': lp_[4], 'lp_obj': round(value(lp_[0].obj_func), 3),
-                    'lp_time': round(lp_[1].Solver.Time, 3),
-                    'nlp_var': nlp_[3], 'nlp_cons': nlp_[4], 'nlp_obj': round(value(nlp_[0].obj_func), 3),
-                    'nlp_time': round(nlp_[1].Solver.Time, 3), 'i_max': lp_[2][5]}
     return ins_data
 
 if __name__ == '__main__':
     # instance values = [ndrones, condition, slot, charge)
 
     SB = [3, 'SB', 5, 180]  # 12 nodes including iDL and DP
-
-    SB = [3, 'SB', 5, 120]  # 12 nodes including iDL and DP
-
     RS = [3, 'RS', 5, 360]  # 11 nodes including iDL and DP
-    LA = [4, 'LA', 10, 360]  # 37 nodes including iDL and DP
+    LA = [4, 'LA', 10, 360]  # 27 nodes including iDL and DP
     SB_RS = [4, 'SB_RS', 7, 360]  # 22 nodes including iDLs and DP
     SB_LA = [6, 'SB_LA', 8, 720]  # 48 nodes including iDLs and DP
     RS_LA = [3, 'RS_LA', 15, 720]  # 47 nodes including iDLs and DP
     SB_RS_LA = [5, 'SB_RS_LA', 11, 720]  # 58 nodes including iDLs and DP (now 50)
 
 
-    num_drones = [3, 4, 5]
-    num_slots = [5, 4, 3]
+    num_drones = [4, 5, 6, 7]
+    num_slots = [9, 8, 7, 6]
     collective_data = pd.DataFrame(columns=['city','Iter','drones','slots','lp_var','lp_cons','lp_obj','lp_time','nlp_var','nlp_cons','nlp_obj','nlp_time', 'hx_obj', 'hx_time', 'i_max', 'seed'])
     # for i in range(len(num_slots)):
-    for i in range(3):
-        instance_ = [num_drones[i], 'SB', num_slots[i], 180]
-        for iter_ in range(2):
+    for i in range(4):
+        instance_ = [num_drones[i], 'LA', num_slots[i], 360]
+        for iter_ in range(10):
             seed1 = random.randrange(sys.maxsize)
             # seed1 = [7398268232918813430, 5212190717316351545, 1668143899688653983, 2581989939053518379, 2377513126356229062, 9100199037133970413]
             random.seed(seed1)
@@ -260,33 +229,3 @@ if __name__ == '__main__':
             file_path = os.path.join(current_directory, filename)
             collective_data.to_csv(file_path, index=False)
             collective_data.to_excel("collective_data.xlsx", index=False, engine='openpyxl')
-
-
-    num_drones = [4, 5, 6, 7, 8]
-    num_slots = [10, 8, 7, 6, 5]
-    collective_data = pd.DataFrame(columns=['city', 'Iter', 'drones', 'slots', 'lp_var', 'lp_cons', 'lp_obj', 'lp_time', 'nlp_var', 'nlp_cons',
-                 'nlp_obj', 'nlp_time', 'i_max', 'seed'])
-    # collective_data = pd.DataFrame(columns=['city','Iter','drones','slots','lp_var','lp_cons','lp_obj','lp_time','nlp_var','nlp_cons','nlp_obj','nlp_time'])
-    for i in range(len(num_slots)):
-        instance_ = [num_drones[i], 'LA', num_slots[i], 120]
-        for iter_ in range(10):
-            seed1 = random.randrange(sys.maxsize)
-            random.seed(seed1)
-            print(i, ': seed === ', seed1)
-            run(instance_, verbose=True)
-            sol_ = compare(SB, report=False, collective_report= True)
-            new_row = {
-                'city': instance_[1], 'Iter': 1+iter_, 'drones': num_drones[i], 'slots': num_slots[i], 'lp_var': sol_['lp_var'], 'lp_cons': sol_['lp_cons'], 'lp_obj': sol_['lp_obj'],
-                'lp_time': sol_['lp_time'], 'nlp_var': sol_['nlp_var'], 'nlp_cons': sol_['nlp_cons'], 'nlp_obj': sol_['nlp_obj'], 'nlp_time': sol_['nlp_time'], 'i_max': str(sol_['i_max']), 'seed': str(seed1)}
-            collective_data = pd.concat([collective_data, pd.DataFrame([new_row])], ignore_index=True)
-            current_directory = os.getcwd()
-            filename = 'collective_data'+'_'+time.strftime("%H%M%S")+'.csv'
-            file_path = os.path.join(current_directory, filename)
-            collective_data.to_csv(file_path, index=False)
-            collective_data.to_excel("collective_data.xlsx", index=False, engine='openpyxl')
-
-
-    # Options:
-    # Control the verbosity of the solvers by changing the verbose=True/False
-    # If you want to record the solution, change the report=True ==>
-    # makes a .txt file to record the current comparison of the solutions
