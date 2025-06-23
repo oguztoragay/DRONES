e0ec80837bc3325095f241783af5ae676008478e
Oguz Toragay <ozt0008@auburn.edu>
2025-06-22 21:57:10 -0400

    Jun_19_Submission
    Jun_19_Submission

--- ARCHIVE/Nonlinear/NonlinearModel.py
+++
@@ -1,4 +0,0 @@
-# Started: 04/29/2024
-# This is the code for the nonlinear model defined in the 4th section of the paper.
-# To be able to use the full capabilities of Gurobi in solving nonlinear and quadratic models, this model has been coded using Gurobipy.
-
