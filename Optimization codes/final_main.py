3ab0974b030389404f27ae8b2455ce11f3257473
Oguz Toragay <ozt0008@auburn.edu>
2025-06-30 15:08:40 -0400

    6/30

--- Optimization codes/final_main.py
+++ Optimization codes/final_main.py
@@ -95,39 +95,39 @@ def compare(instance, report, collective_report):
     except:
         print('No pickle file')
 
-    # print('~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance ~~~~~~~~~~~~~~~~~~~~~~~~~')
-    # print('     seed:', seed1)  # nlp_[2][12])
-    # print(' Families:', nlp_[2][7])
-    # print('  i_times:', nlp_[2][5])
-    # print('Fam slots:', nlp_[2][11])
-    # print('Ear_dates:', nlp_[2][9])
-    # print('Due_dates:', nlp_[2][1])
-    # print('Monitor_t:', nlp_[2][2])
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance ~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('     seed:', seed1)  # nlp_[2][12])
+    print(' Families:', nlp_[2][7])
+    print('  i_times:', nlp_[2][5])
+    print('Fam slots:', nlp_[2][11])
+    print('Ear_dates:', nlp_[2][9])
+    print('Due_dates:', nlp_[2][1])
+    print('Monitor_t:', nlp_[2][2])
 
-    # print('~~~~~~~~~~~~~~~~~~~ Comparing the results ~~~~~~~~~~~~~~~~~~~')
-    # print('***** lp_objective:', value(lp_[0].obj_func))
-    # # print('***** lp_Sol_time:', round(lp_[1].Solver.Wall_time, 3))
+    print('~~~~~~~~~~~~~~~~~~~ Comparing the results ~~~~~~~~~~~~~~~~~~~')
+    print('***** lp_objective:', value(lp_[0].obj_func))
+    # print('***** lp_Sol_time:', round(lp_[1].Solver.Wall_time, 3))
 
-    # for i in range(0, lassign_list.shape[0]):
-    #     print('     Drone (' + str(i + 1) + '):')
-    #     print(''.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lassign_list[i]))
-    #     print('s_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lps_values[i]))
-    #     print('c_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lpc_values[i]))
-    #     print('travels'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lptr_values[i]))
-    #     print('charges'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lpt_values[i]))
-    #     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    for i in range(0, lassign_list.shape[0]):
+        print('     Drone (' + str(i + 1) + '):')
+        print(''.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lassign_list[i]))
+        print('s_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lps_values[i]))
+        print('c_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lpc_values[i]))
+        print('travels'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lptr_values[i]))
+        print('charges'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in lpt_values[i]))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-    # print('***** nlp_objective:', value(nlp_[0].obj_func))
-    # # print('***** nlp_Sol_time:', round(nlp_[1].Solver.Wall_time, 3))
-    #
-    # for i in range(0, assign_list.shape[0]):
-    #     print('     Drone (' + str(i + 1) + '):')
-    #     print(''.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in assign_list[i]))
-    #     print('s_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlps_values[i]))
-    #     print('c_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlpc_values[i]))
-    #     print('travels'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlptr_values[i]))
-    #     print('charges'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlpt_values[i]))
-    #     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('***** nlp_objective:', value(nlp_[0].obj_func))
+    # print('***** nlp_Sol_time:', round(nlp_[1].Solver.Wall_time, 3))
+
+    for i in range(0, assign_list.shape[0]):
+        print('     Drone (' + str(i + 1) + '):')
+        print(''.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in assign_list[i]))
+        print('s_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlps_values[i]))
+        print('c_times'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlpc_values[i]))
+        print('travels'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlptr_values[i]))
+        print('charges'.ljust(col_widths), " | ".join(str(item).ljust(col_widths) for item in nlpt_values[i]))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
     print('***** hx_objective:', h_obj_value)
     print('***** hx_Sol_time:', h_sol_time)
@@ -142,59 +142,59 @@ def compare(instance, report, collective_report):
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
-    # print('==========================================')
+    print('==========================================')
 
-    # if report:
-    #     folder_name = 'res_'+str(date.today())+'_Major'
-    #     f_loc = os.getcwd()
-    #     directory_path = os.path.join(f_loc, 'Comparison_modify', 'Results', folder_name)
-    #     os.makedirs(directory_path, exist_ok=True)
-    #     # file name: City + number of drones + number of slots + max charge
-    #     with open(directory_path+'/'+instance[1]+'_'+str(instance[0])+'_'+str(instance[2])+'_'+str(instance[3])+'_'+time.strftime("%H%M%S")+'.txt', 'w+') as file:
-    #         file.write('~~~~~~~~~~~~~~~~~~~ ' + str(instance) + ' ~~~~~~~~~~~~~~~~~~~\n')
-    #         file.write('     seed: ' + str(seed1)+'\n')  #  str(nlp_[2][12])
-    #         file.write(' Families: ' + str(nlp_[2][7])+'\n')
-    #         file.write('  i_times: ' + str(nlp_[2][5])+'\n')
-    #         file.write('Fam slots: ' + str(nlp_[2][11])+'\n')
-    #         file.write('Ear_dates: ' + str(nlp_[2][9])+'\n')
-    #         file.write('Due_dates: ' + str(nlp_[2][1])+'\n')
-    #         file.write('Monitor_t: ' + str(nlp_[2][2])+'\n')
-    #         file.write('~~~~~~~~~~~~~~~~~~~ Comparing the results ~~~~~~~~~~~~~~~~~~~\n')
-    #         file.write(f'***** lp_Variables: {lp_[3]}\n')
-    #         file.write(f'***** lp_Constraints: {lp_[4]}\n')
-    #         file.write(f'***** lp_objective: {value(lp_[0].obj_func)}\n')
-    #         # file.write(f'***** lp_Sol_time: {round(lp_[1].Solver.Time, 3)}\n')
-    #         col_widths = 10
-    #         for i in range(0, lassign_list.shape[0]):
-    #             file.write(f'     Drone ({i + 1}):\n')
-    #             file.write(
-    #                 ''.ljust(col_widths) + " | ".join(str(item).ljust(col_widths) for item in lassign_list[i]) + '\n')
-    #             file.write('s_times'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in lps_values[i]) + '\n')
-    #             file.write('c_times'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in lpc_values[i]) + '\n')
-    #             file.write('travels'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in lptr_values[i]) + '\n')
-    #             file.write('charges'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in lpt_values[i]) + '\n')
-    #             file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-    #         file.write(f'***** nlp_Variables: {nlp_[3]}\n')
-    #         file.write(f'***** nlp_Constraints: {nlp_[4]}\n')
-    #         file.write(f'***** nlp_objective: {value(nlp_[0].obj_func)}\n')
-    #         # file.write(f'***** nlp_Sol_time: {round(nlp_[1].Solver.Time, 3)}\n')
-    #         for i in range(0, assign_list.shape[0]):
-    #             file.write(f'     Drone ({i + 1}):\n')
-    #             file.write(
-    #                 ''.ljust(col_widths) + " | ".join(str(item).ljust(col_widths) for item in assign_list[i]) + '\n')
-    #             file.write('s_times'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in nlps_values[i]) + '\n')
-    #             file.write('c_times'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in nlpc_values[i]) + '\n')
-    #             file.write('travels'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in nlptr_values[i]) + '\n')
-    #             file.write('charges'.ljust(col_widths) + " | ".join(
-    #                 str(item).ljust(col_widths) for item in nlpt_values[i]) + '\n')
-    #             file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    if report:
+        # folder_name = 'res_'+str(date.today())+'_Major'
+        f_loc = os.getcwd()
+        directory_path = os.path.join(f_loc, 'Results')
+        os.makedirs(directory_path, exist_ok=True)
+        # file name: City + number of drones + number of slots + max charge
+        with open(directory_path+'/'+instance[1]+'_'+str(instance[0])+'_'+str(instance[2])+'_'+str(instance[3])+'_'+time.strftime("%H%M%S")+'.txt', 'w+') as file:
+            file.write('~~~~~~~~~~~~~~~~~~~ ' + str(instance) + ' ~~~~~~~~~~~~~~~~~~~\n')
+            file.write('     seed: ' + str(seed1)+'\n')  #  str(nlp_[2][12])
+            file.write(' Families: ' + str(nlp_[2][7])+'\n')
+            file.write('  i_times: ' + str(nlp_[2][5])+'\n')
+            file.write('Fam slots: ' + str(nlp_[2][11])+'\n')
+            file.write('Ear_dates: ' + str(nlp_[2][9])+'\n')
+            file.write('Due_dates: ' + str(nlp_[2][1])+'\n')
+            file.write('Monitor_t: ' + str(nlp_[2][2])+'\n')
+            file.write('~~~~~~~~~~~~~~~~~~~ Comparing the results ~~~~~~~~~~~~~~~~~~~\n')
+            file.write(f'***** lp_Variables: {lp_[3]}\n')
+            file.write(f'***** lp_Constraints: {lp_[4]}\n')
+            file.write(f'***** lp_objective: {value(lp_[0].obj_func)}\n')
+            # file.write(f'***** lp_Sol_time: {round(lp_[1].Solver.Time, 3)}\n')
+            col_widths = 10
+            for i in range(0, lassign_list.shape[0]):
+                file.write(f'     Drone ({i + 1}):\n')
+                file.write(
+                    ''.ljust(col_widths) + " | ".join(str(item).ljust(col_widths) for item in lassign_list[i]) + '\n')
+                file.write('s_times'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in lps_values[i]) + '\n')
+                file.write('c_times'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in lpc_values[i]) + '\n')
+                file.write('travels'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in lptr_values[i]) + '\n')
+                file.write('charges'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in lpt_values[i]) + '\n')
+                file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+            file.write(f'***** nlp_Variables: {nlp_[3]}\n')
+            file.write(f'***** nlp_Constraints: {nlp_[4]}\n')
+            file.write(f'***** nlp_objective: {value(nlp_[0].obj_func)}\n')
+            # file.write(f'***** nlp_Sol_time: {round(nlp_[1].Solver.Time, 3)}\n')
+            for i in range(0, assign_list.shape[0]):
+                file.write(f'     Drone ({i + 1}):\n')
+                file.write(
+                    ''.ljust(col_widths) + " | ".join(str(item).ljust(col_widths) for item in assign_list[i]) + '\n')
+                file.write('s_times'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in nlps_values[i]) + '\n')
+                file.write('c_times'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in nlpc_values[i]) + '\n')
+                file.write('travels'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in nlptr_values[i]) + '\n')
+                file.write('charges'.ljust(col_widths) + " | ".join(
+                    str(item).ljust(col_widths) for item in nlpt_values[i]) + '\n')
+                file.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
     if collective_report:
         print('I am generating collective report.')
         ins_data = {'lp_var': 00, 'lp_cons': 00, 'lp_obj': 00, 'lp_time': 00, 'nlp_var': 00, 'nlp_cons': 00, 'nlp_obj': 00, 'nlp_time': 00,
@@ -217,6 +217,7 @@ def compare(instance, report, collective_report):
 
 if __name__ == '__main__':
 
+# ----> Info abut the instances:
     # SB = 10 Real nodes + 1 iDL + 1 DP
     # RS = 9 Real nodes + 1 iDL + 1 DP
     # LA = 27 Real nodes + 1 iDL + 1 DP
@@ -224,15 +225,18 @@ if __name__ == '__main__':
     # SB_LA = 37 Real nodes + 2 iDLs + 1 DP
     # RS_LA = 36 Real nodes + 2 iDLs + 1 DP
     # SB_RS_LA = 46 Real nodes + 3 iDLs + 1 DP
-    # LA: 4,8 and 5,8
-    # SB-RS-LA: 8,8 and 9,7
-    num_drones = [7, 8, 9, 9]
-    num_slots = [9, 9, 8, 7]
+
+    # ----> Enter the number of drones and slots in the following two lines
+    num_drones = [4]
+    num_slots = [7]
+
     collective_data = pd.DataFrame(columns=['city','Iter','drones','slots','lp_var','lp_cons','lp_obj','lp_time','nlp_var','nlp_cons','nlp_obj','nlp_time', 'hx_obj', 'hx_time', 'i_max', 'seed'])
-    # for i in range(len(num_slots)):
-    for i in range(4):
-        instance_ = [num_drones[i], 'RS_LA', num_slots[i], 360]
-        for iter_ in range(10):
+
+    for i in range(len(num_slots)):
+        # ----> Enter the instance name in the following line
+        instance_ = [num_drones[i], 'SB', num_slots[i], 360]
+        # ----> Enter the number of randomly generated instances in the following line
+        for iter_ in range(1):
             for jf in ['lp.pickle', 'nlp.pickle', 'hexa.pickle']:
                 if os.path.exists(jf):
                     os.remove(jf)
@@ -240,11 +244,15 @@ if __name__ == '__main__':
                 else:
                     print(f"{jf} does not exist.")
             seed1 = random.randrange(sys.maxsize)
+
+            # ----> To fix the seed uncomment the following line and enter a numeric value
             # seed1 = 4116131705355157001
+
             random.seed(seed1)
             print('Iteration:', (10*i) + iter_+1, '====> seed = ', seed1)
-            run(instance_, verbose=True, seed=seed1, sol_time=3600)
-            sol_ = compare(instance_, report=False, collective_report=True)
+            # ----> Adjust the solution time for all the models in the following line
+            run(instance_, verbose=True, seed=seed1, sol_time=15)
+            sol_ = compare(instance_, report=True, collective_report=True)
             new_row = {
                 'city': instance_[1], 'Iter': 1+iter_, 'drones': num_drones[i], 'slots': num_slots[i],
                 'lp_var': sol_['lp_var'], 'lp_cons': sol_['lp_cons'], 'lp_obj': sol_['lp_obj'], 'lp_time': sol_['lp_time'],
@@ -252,6 +260,11 @@ if __name__ == '__main__':
                 'hx_obj': sol_['hx_obj'], 'hx_time': sol_['hx_time'], 'i_max': str(sol_['i_max']), 'seed': [seed1]}
             collective_data = pd.concat([collective_data, pd.DataFrame([new_row])], ignore_index=True)
             current_directory = os.getcwd()
+
+            f_loc = os.getcwd()
+            directory_path = os.path.join(f_loc, 'Results')
+            os.makedirs(directory_path, exist_ok=True)
+
             filename = 'collective_data'+'_'+str(date.today())+time.strftime("%H%M%S")+'.csv'
             file_path = os.path.join(current_directory, filename)
             collective_data.to_csv(file_path, index=False)
